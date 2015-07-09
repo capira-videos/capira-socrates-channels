ac4359500b0e43eff3b249002722c2189d9e9229
Robin Linus <robin_woll@capira.de>
2015-07-10 01:31:06 +0200

    data-binding

---
+++ app/scripts/folder.js
@@ -0,0 +1,113 @@
+window.channel={
+	"id": 1,
+	"title": "Mathe-Crashkurs",
+	"parent": 140,
+	"description": "Grundrechenarten, Umformen, Mengen, Potenzen, Logarithmen, Trigonometrie, Sinus",
+	"folders": [{
+		"id": 103,
+		"title": "01: Grundrechenarten, Ausdr\u00fccke, Umformungen",
+		"viewIndex": 0,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "vJcHp9j8U_s",
+		"progress": null
+	}, {
+		"id": 104,
+		"title": "02: Bruchrechnung",
+		"viewIndex": 1,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "QTDEtQn5Iuo",
+		"progress": null
+	}, {
+		"id": 105,
+		"title": "03: Proportionalit\u00e4t, Dreisatz, lineare Funktionen",
+		"viewIndex": 2,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "Djw1nV2Sh4M",
+		"progress": null
+	}, {
+		"id": 106,
+		"title": "04: St\u00fcckweise Definition, Verkettung von Funktionen",
+		"viewIndex": 3,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "2avbpGeFwAM",
+		"progress": null
+	}, {
+		"id": 107,
+		"title": "05: Potenzen, Wurzeln, Exponentialfunktionen, Logarithmen",
+		"viewIndex": 4,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "BAJUm9me6wI",
+		"progress": null
+	}, {
+		"id": 108,
+		"title": "06: Dreiecke, Sinus und Co",
+		"viewIndex": 5,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "QFxZ_sfDumE",
+		"progress": null
+	}, {
+		"id": 109,
+		"title": "07: Elementare L\u00e4ngen, Fl\u00e4chen und Volumen",
+		"viewIndex": 6,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "_bYAHUaazJ0",
+		"progress": null
+	}, {
+		"id": 110,
+		"title": "08: Vektoren",
+		"viewIndex": 7,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "YNjWRLMaE3U",
+		"progress": null
+	}, {
+		"id": 111,
+		"title": "09: Mengen, Zahlenbereiche",
+		"viewIndex": 8,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "etYxOVHr6Wg",
+		"progress": null
+	}, {
+		"id": 112,
+		"title": "10: Ableitungen",
+		"viewIndex": 9,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "4y-RdxwWmW0",
+		"progress": null
+	}, {
+		"id": 113,
+		"title": "11: Gleichungen und Gleichungssysteme, Polynome",
+		"viewIndex": 10,
+		"published": 1,
+		"parent": 1,
+		"description": "",
+		"thumbnail": "gWSw6mDZ8_E",
+		"progress": null
+	}],
+	"units": [],
+	"path": [{
+		"id": 1,
+		"title": "Mathe-Crashkurs"
+	}],
+	"channelAdmin": false,
+	"admin": false
+}
